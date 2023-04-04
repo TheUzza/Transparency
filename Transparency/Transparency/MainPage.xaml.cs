3e197788fd92e5bcd64268431966355d2b24d0bc
Ahmad Aloussi <salamsyr12213@gmail.com>
2023-04-04 05:31:05 +0200

    db connection

---
+++ Transparency/Transparency/MainPage.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using ZXing.Net.Mobile.Forms;
+using MySqlConnector;
+using Transparency;
+
+namespace Tranparency
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        protected override async void OnAppearing()
+        {
+            base.OnAppearing();
+            MySqlDataBase dbConnection = new MySqlDataBase();
+            await dbConnection.CreateTableAsync();
+        }
+
+
+        private async void ScanButton_Clicked(object sender, EventArgs e)
+        {
+            var scanPage = new ZXingScannerPage();
+            await Navigation.PushAsync(scanPage);
+
+            scanPage.OnScanResult += (result) =>
+            {
+                Device.BeginInvokeOnMainThread(async () =>
+                {
+                    await Navigation.PopAsync();
+                    await DisplayAlert("QR Code Value", result.Text, "OK");
+                });
+            };
+        }
+
+        private async void ProcessQrCodeResult(string qrCodeData)
+        {
+            try
+            {
+                MySqlDataBase dbConnection = new MySqlDataBase();
+                using (var connection = dbConnection.GetConnection())
+                {
+                    await connection.OpenAsync();
+
+                    string query = "INSERT INTO my_table (qr_code_data) VALUES (@qrCodeData);";
+                    using (var command = new MySqlCommand(query, connection))
+                    {
+                        command.Parameters.AddWithValue("@qrCodeData", qrCodeData);
+                        await command.ExecuteNonQueryAsync();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+        }
+    }
+}
---
+++ Transparency/Transparency/MySqlDataBase.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Threading.Tasks;
+using MySqlConnector;
+
+namespace Transparency
+{
+	public class MySqlDataBase
+	{
+        private string connectionString =
+         "server=gsw.dnsalias.net;uid=brandtransparency;pwd=db23/M3rm2EA;database=brandtransparency";
+
+        public MySqlConnection GetConnection()
+        {
+            return new MySqlConnection(connectionString);
+        }
+
+
+        public async Task CreateTableAsync()
+        {
+            try
+            {
+                using (var connection = GetConnection())
+                {
+                    await connection.OpenAsync();
+
+                    string createTableQuery = @"CREATE TABLE IF NOT EXISTS test (
+                                            id INT AUTO_INCREMENT PRIMARY KEY,
+                                            qr_code_data TEXT NOT NULL
+                                        );";
+
+                    using (var command = new MySqlCommand(createTableQuery, connection))
+                    {
+                        await command.ExecuteNonQueryAsync();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+        }
+
+    }
+}
+
